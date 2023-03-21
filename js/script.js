c448974084f880f9b7b8d96812f44849e4207d3a
leticea <nascimento.leticeah@gmail.com>
2023-03-21 11:39:34 -0300

    feat(script.js): creating handleMouseDown function

--- js/script.js
+++ js/script.js
@@ -1,9 +1,14 @@
 const keys = document.querySelectorAll(".key");
 
-const handleClick = (key) => {
+const handleMouseDown = (key) => {
   key.style.background = "#ddd";
 }
 
+const handleMouseUp = (key) => {
+  key.style.background = "#fff";
+}
+
 keys.forEach((key) => {
-  key.addEventListener("click", () => handleClick(key));
+  key.addEventListener("mousedown", () => handleMouseDown(key));
+  key.addEventListener("mouseup", () => handleMouseUp(key));
 });
