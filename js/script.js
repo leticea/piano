05c7db42cd87b0f736b6baae8232bf1b4f52617a
leticea <nascimento.leticeah@gmail.com>
2023-03-21 11:35:28 -0300

    feat(script.js): creating button functions

---
+++ js/script.js
@@ -0,0 +1,9 @@
+const keys = document.querySelectorAll(".key");
+
+const handleClick = (key) => {
+  key.style.background = "#ddd";
+}
+
+keys.forEach((key) => {
+  key.addEventListener("click", () => handleClick(key));
+});
