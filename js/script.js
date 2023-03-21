22191979fdd121a784862e080cd05a59d6e56ee0
leticea <nascimento.leticeah@gmail.com>
2023-03-21 11:53:40 -0300

    feat(script.js): creating handleMouseDown function

--- js/script.js
+++ js/script.js
@@ -1,12 +1,22 @@
 const keys = document.querySelectorAll(".key");
 
 const handleMouseDown = (key) => {
+  if (key.className.includes("black")) {
+    key.classList.add("black-pressed");
+    return;
+  }
+
   key.style.background = "#ddd";
-}
+};
 
 const handleMouseUp = (key) => {
+  if (key.className.includes("black")) {
+    key.classList.remove("black-pressed");
+    return;
+  }
+
   key.style.background = "#fff";
-}
+};
 
 keys.forEach((key) => {
   key.addEventListener("mousedown", () => handleMouseDown(key));
